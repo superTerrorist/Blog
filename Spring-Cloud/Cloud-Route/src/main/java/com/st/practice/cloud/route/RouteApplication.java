c855e795743cde65397d41969dec09f85e315015
wangguanjie <wangguanjie@gengee.cn>
2021-03-25 09:53:10 +0800

    [Feature] spring cloud example

---
+++ Spring-Cloud/Cloud-Route/src/main/java/com/st/practice/cloud/route/RouteApplication.java
@@ -0,0 +1,14 @@
+package com.st.practice.cloud.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author David Wang <https://github.com/superTerrorist>
+ */
+@SpringBootApplication
+public class RouteApplication{
+    public static void main(String[] args){
+        SpringApplication.run(RouteApplication.class,args);
+    }
+}
